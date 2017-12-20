9c974cb2ba0eb94854a6952d54765decbe0795d5
haocao <caohaoch@gmail.com>
2017-12-20 15:29:39 +0800

    Refactor package names.

---
+++ elastic-job-example-jobs/src/main/java/io/elasticjob/example/job/simple/JavaSimpleJob.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.elasticjob.example.job.simple;
+
+import io.elasticjob.api.ShardingContext;
+import io.elasticjob.api.simple.SimpleJob;
+import io.elasticjob.example.fixture.entity.Foo;
+import io.elasticjob.example.fixture.repository.FooRepository;
+import io.elasticjob.example.fixture.repository.FooRepositoryFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class JavaSimpleJob implements SimpleJob {
+    
+    private FooRepository fooRepository = FooRepositoryFactory.getFooRepository();
+    
+    @Override
+    public void execute(final ShardingContext shardingContext) {
+        System.out.println(String.format("Item: %s | Time: %s | Thread: %s | %s",
+                shardingContext.getShardingItem(), new SimpleDateFormat("HH:mm:ss").format(new Date()), Thread.currentThread().getId(), "SIMPLE"));
+        List<Foo> data = fooRepository.findTodoData(shardingContext.getShardingParameter(), 10);
+        for (Foo each : data) {
+            fooRepository.setCompleted(each.getId());
+        }
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
